e8477865d171a10ccee6068496a6011516c42d4d
Gkumar <gk9801421579@gmail.com>
2021-09-01 21:26:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3');
+audio.loop = true;
+audio.play();
+// try{
+    
+// }
+// catch(err){
+//     console.log("no play"+err);
+// }
